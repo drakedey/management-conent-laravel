c55a1c065871a0e726ebbd2394c6280597b0dd74
Ronalfonso <ronald.alfonso@tuten.cl>
2019-06-18 13:49:30 -0400

    API tag

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -10,12 +10,28 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * @OA\Info(title="API Vespek", version="1.0")
+ *
+ * @OA\Server(url="http://127.0.0.1:8000")
+ */
+
 class TagController extends Controller
 {
+
     /**
-     * Display a listing of the resource.
-     *
-     * @return void
+     * @OA\Get(
+     *     path="/tags",
+     *     summary="Mostrar Tags",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Mostrar todos los tags."
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="Ha ocurrido un error."
+     *     )
+     * )
      */
     public function index(Request $request)
     {
@@ -53,7 +69,7 @@ class TagController extends Controller
             $tag_content = new TagContent();
             $tag_content->name = $request->name;
             $tag_content->tag_id = $tag->id;
-            $tag_content->language_id = $request->languaje_id;
+            $tag_content->language_id = $request->language_id;
             $tag_content->save();
 
             return response()->json(['tag' => $tag], 200);
@@ -87,6 +103,7 @@ class TagController extends Controller
     public function update(Request $request, $id)
     {
         $tag = Tag::query()->findOrFail($id);
+        $tag_content = TagContent::query()->find($request->tag_id);
 
         $rules = [
           'name' => 'unique:tags_content',
@@ -94,26 +111,25 @@ class TagController extends Controller
 
 
         $data = $request->all();
-
         $validator = Validator::make($data,$rules);
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 400);
         }
 
-        $content = $data['name'];
-
-
         if ($request->has('name')) {
-            $content->name = $content['name'];
+            $tag_content->name = $request->name;
+            $tag_content->update();
         }
+
 
         if (!$tag->isDirty()) {
             return response()->json(['error' => 'You must specify at least one different value to update', 'code' => 422], 422);
         }
 
-        $tag->save();
 
-        return response()->json(['success' => 'country updated succesfully', 'country' => $tag], 200);
+
+
+        return response()->json(['success' => 'tag updated succesfully', 'tag' => $tag], 200);
     }
 
     /**
