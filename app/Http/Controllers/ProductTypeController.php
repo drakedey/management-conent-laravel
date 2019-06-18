38a14e74dc09525d28ad0ba76528cda590092df0
Joskar Hernandez <joskar.hernandez@tuten.cl>
2019-06-17 23:54:28 -0400

    Feature: CRUD product type

--- app/Http/Controllers/ProductTypeController.php
+++ app/Http/Controllers/ProductTypeController.php
@@ -77,8 +77,10 @@ class ProductTypeController extends Controller
     public function update(int $id, Request $request)
     {
 
-        $rules = [
+        $productType = ProductType::findOrFail($id);
 
+        $rules = [
+            'content' => 'required'
         ];
 
         $data = $request->all();
@@ -87,8 +89,36 @@ class ProductTypeController extends Controller
             return response(['error' => $validator->errors()], 400);
         }
 
+        $content = $data['content'];
+        //Validating that the language exists on DB and the content is not empty and accomplish with the minimun data
+        foreach ($content as $language => $language_content) {
+            //Validating thar exists in other case returns 404
+            Language::where('name', '=', $language)->firstOrFail();
+            $rules = [
+                'name' => 'required',
+            ];
+            $innerContentValidator = Validator::make($language_content, $rules);
+            if($innerContentValidator->fails()) {
+                return response(['error' => $innerContentValidator->errors()], 400);
+            }
+        }
 
-        return response(['success' => 'language updated succesfully'], 200);
+        foreach ($content as $language => $language_content) {
+            $languageTarget = Language::where('name', '=', $language)->first();
+            $contentTarget = ProducTypeLanguages::where('type_id', '=', $productType->id)->where('language_id','=', $languageTarget->id)->first();
+            $productTypeLanguage = [
+                'name' => $language_content['name'],
+            ];
+            if($contentTarget) {
+                $contentTarget->update($productTypeLanguage);
+            } else {
+                $productTypeLanguage['type_id'] = $productType->id;
+                $productTypeLanguage['language_id'] = $languageTarget->id;
+                ProducTypeLanguages::create($productTypeLanguage);
+            }
+        }
+
+        return response(['success' => 'language updated succesfully', 'productType' => $productType], 200);
     }
 
     public function show(int $id) {
@@ -97,7 +127,42 @@ class ProductTypeController extends Controller
     }
 
     public function destroy(int $id, Request $request) {
+        $productType = ProductType::findOrFail($id);
+        $data = $request->all();
+        $rules = [
+            'all' => 'required',
+        ];
 
-        return response([], 200);
+        $validator = Validator::make($data, $rules);
+        if($validator->fails()) {
+            return response(['errors' => $validator->errors()], 400);
+        }
+
+        $data['all'] = $data['all'] == 'true' ? true : false;
+
+        if($data['all'] == true) {
+            $productType->delete();
+            return response(['success' => 'product type deleted', 'productType' => $productType], 200);
+        } else {
+            //Validating
+            $rules = [
+                'languageTargets' => 'required'
+            ];
+            $validator = Validator::make($data, $rules);
+            if($validator->fails()) {
+                return response(['errors' => $validator->errors()], 400);
+            }
+            foreach ($data['languageTargets'] as $language) {
+                $languageTarget = Language::where('name', '=', $language)->firstOrFail();
+                ProducTypeLanguages::where('type_id', '=', $productType->id)->where('language_id','=', $languageTarget->id)->firstOrFail();
+            }
+            //Deleting
+            foreach ($data['languageTargets'] as $language) {
+                $languageTarget = Language::where('name', '=', $language)->firstOrFail();
+                $productContent = ProducTypeLanguages::where('type_id', '=', $productType->id)->where('language_id','=', $languageTarget->id)->firstOrFail();
+                $productContent->delete();
+            }
+            return response(['success' => 'product type languages deleted', 'productType' => $productType], 200);
+        }
     }
 }
